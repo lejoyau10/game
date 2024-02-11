0653868b618b02be4f2a416c49b624fcd3c68b50
lejoyau10 <lejoyau10@gmail.com>
2024-02-11 14:27:56 +0300

    node learn node JS.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ readFile('./content/first.txt', 'utf8', (err,result)=>{
             if(err){
                 console.log(err);
                 return;
-            }console.log(result)
+            }console.log(result) 
         })
     })
 })
